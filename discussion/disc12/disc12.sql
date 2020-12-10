69e311bb3be50a54e66c8aedbe68ee1fa1d21e37
S1n4rchenemy <sungmingtien@gmail.com>
2020-12-10 16:45:20 -0600

    discussion 12 revised based on the answers

--- discussion/disc12/disc12.sql
+++ discussion/disc12/disc12.sql
@@ -30,6 +30,11 @@ SELECT a.name, b.name FROM records AS a, records AS b, meetings AS c, meetings A
           a.name > b.name;
 
 
+-- Problem 3.3 (revised after referring to the solution)
+
+-- "No.  If a department has multiple meetings, then all pairs of individuals within that department will be listed multiple times. To avoid this, we can use the DISTINCT keyword."
+
+
 -- Problem 3.4
 
 SELECT a.name FROM records AS a, records AS b 
@@ -49,30 +54,38 @@ SELECT a.day FROM meetings AS a, records AS b
     GROUP BY a.day HAVING count(*) < 5;
 
 
--- Problem 4.3
+-- Problem 4.3 
+-- (Original answer was completely wrong: misunderstood the question)
 
-SELECT division FROM records GROUP BY division HAVING count(*) > 1 AND SUM(salary) < 100000;
+SELECT a.division FROM records AS a, records AS b 
+    WHERE a.division = b.division AND 
+          a.name > b.name
+    GROUP BY a.division  
+    HAVING MAX(a.salary + b.salary) < 100000;
 
 
--- Problem 5.1
+-- Problem 5.1 
+-- (Original answer was not perfect: missiong the head name for each column)
 
 CREATE TABLE num_taught AS 
-    SELECT professor, course, count(*) FROM courses GROUP BY professor, course;
+    SELECT professor AS professor, course AS course, count(*) AS times 
+    FROM courses GROUP BY professor, course;
 
 
 -- Problem 5.2
 
 SELECT a.professor, b.professor, a.course FROM num_taught AS a, num_taught AS b
     WHERE a.course = b.course AND 
-          a."count(*)" = b."count(*)" AND 
+          a.times = b.times AND 
           a.professor > b.professor;
 
 
 -- Problem 5.3 
+-- (Original answer was wrong in the GROUP BY expression: missing the necessary condition of the same course)
 
 SELECT a.professor, b.professor FROM courses AS a, courses AS b 
     WHERE a.course = b.course AND 
           a.semester = b.semester AND 
           a.professor > b.professor 
-    GROUP BY a.professor, b.professor 
+    GROUP BY a.professor, b.professor, a.course 
     HAVING count(*) > 1;
