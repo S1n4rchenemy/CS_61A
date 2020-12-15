2ad3f4dcc2158a42d2f33de33d95dd90ecf878e9
S1n4rchenemy <sungmingtien@gmail.com>
2020-12-14 23:07:49 -0600

    lab14 all done

---
+++ lab/lab14/lab14.scm
@@ -0,0 +1,16 @@
+(define (split-at lst n)
+  (cond
+    ((= n 0) (cons '() lst))
+    ((null? (cdr lst)) (cons (cons (car lst) nil) nil))
+    (else 
+      (define rest (split-at (cdr lst) (- n 1)))
+      (cons (cons (car lst) (car rest)) (cdr rest))))
+)
+
+
+(define (compose-all funcs)
+  (if (null? funcs) 
+    (lambda (x) x)
+    (lambda (x) ((compose-all (cdr funcs)) ((car funcs) x))))
+)
+
