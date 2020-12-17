4de921b32b9fcccb2ed4f7d362e7a4039b48e213
S1n4rchenemy <sungmingtien@gmail.com>
2020-12-17 02:03:25 -0600

    disc14 kick off

---
+++ discussion/disc14/disc14.py
@@ -0,0 +1,201 @@
+###############
+# Problem 1.1 #
+###############
+
+def paths(x, y):
+    """Return a list of ways to reach y from x by repeated
+    incrementing or doubling.
+    >>> paths(3, 5)
+    [[3, 4, 5]]
+    >>> sorted(paths(3, 6))
+    [[3, 4, 5, 6], [3, 6]]
+    >>> sorted(paths(3, 9))
+    [[3, 4, 5, 6, 7, 8, 9], [3, 4, 8, 9], [3, 6, 7, 8, 9]]
+    >>> paths(3, 3) # No calls is a valid path
+    [[3]]
+    """
+    if x == y:
+        return [[x]]
+    elif 2 * x > y:
+        return [[x] + lst for lst in paths(x+1, y)]
+    else:
+        a = paths(x+1, y)
+        b = paths(x*2, y)
+        return [[x] + lst for lst in (a + b)]
+
+
+
+###############
+# Problem 1.2 #
+###############
+
+def merge(s1, s2):
+    """ Merges two sorted lists """
+    if len(s1) == 0:
+        return s2
+    elif len(s2) == 0:
+        return s1
+    elif s1[0] < s2[0]:
+        return [s1[0]] + merge(s1[1:], s2)
+    else:
+        return [s2[0]] + merge(s1, s2[1:])
+
+def mergesort(seq):
+    """Merge sort a list
+    >>> mergesort([2, 1, 5, 4, 6])
+    [1, 2, 4, 5, 6]
+    >>> mergesort([4, 3, 2, 1])
+    [1, 2, 3, 4]
+    """
+    if len(seq) == 1:
+        return seq
+    else:
+        a = mergesort(seq[: len(seq) // 2 ])
+        b = mergesort(seq[len(seq) // 2 :])
+        return merge(a, b)
+
+
+###############
+# Problem 2.2 #
+###############
+
+def widest_level(t):
+    """
+    >>> sum([[1], [2]], [])
+    [1, 2]
+    >>> t = Tree(3, [Tree(1, [Tree(1), Tree(5)]), Tree(4, [Tree(9, [Tree(2)])])])
+    >>> widest_level(t)
+    [1, 5, 9]
+    """
+    levels = []
+    x = [t]
+    while False in [t.is_leaf() for t in x]:
+        levels += [[t.label for t in x]]
+        x = sum([t.branches for t in x], [])
+    return max(levels, key=len)
+
+
+
+##############
+# Tree class #
+##############
+
+class Tree:
+    """
+    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+    >>> t.label
+    3
+    >>> t.branches[0].label
+    2
+    >>> t.branches[1].is_leaf()
+    True
+    """
+    def __init__(self, label, branches=[]):
+        for b in branches:
+            assert isinstance(b, Tree)
+        self.label = label
+        self.branches = list(branches)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def map(self, fn):
+        """
+        Apply a function `fn` to each node in the tree and mutate the tree.
+
+        >>> t1 = Tree(1)
+        >>> t1.map(lambda x: x + 2)
+        >>> t1.map(lambda x : x * 4)
+        >>> t1.label
+        12
+        >>> t2 = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+        >>> t2.map(lambda x: x * x)
+        >>> t2
+        Tree(9, [Tree(4, [Tree(25)]), Tree(16)])
+        """
+        self.label = fn(self.label)
+        for b in self.branches:
+            b.map(fn)
+
+    def __contains__(self, e):
+        """
+        Determine whether an element exists in the tree.
+
+        >>> t1 = Tree(1)
+        >>> 1 in t1
+        True
+        >>> 8 in t1
+        False
+        >>> t2 = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+        >>> 6 in t2
+        False
+        >>> 5 in t2
+        True
+        """
+        if self.label == e:
+            return True
+        for b in self.branches:
+            if e in b:
+                return True
+        return False
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            branch_str = ''
+        return 'Tree({0}{1})'.format(self.label, branch_str)
+
+    def __str__(self):
+        def print_tree(t, indent=0):
+            tree_str = '  ' * indent + str(t.label) + "\n"
+            for b in t.branches:
+                tree_str += print_tree(b, indent + 1)
+            return tree_str
+        return print_tree(self).rstrip()
+
+
+##############
+# Link class #
+##############
+
+class Link:
+    """A linked list.
+
+    >>> s = Link(1)
+    >>> s.first
+    1
+    >>> s.rest is Link.empty
+    True
+    >>> s = Link(2, Link(3, Link(4)))
+    >>> s.first = 5
+    >>> s.rest.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s                                    # Displays the contents of repr(s)
+    Link(5, Link(6))
+    >>> s.rest = Link(7, Link(Link(8, Link(9))))
+    >>> s
+    Link(5, Link(7, Link(Link(8, Link(9)))))
+    >>> print(s)                             # Prints str(s)
+    <5 7 <8 9>>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
